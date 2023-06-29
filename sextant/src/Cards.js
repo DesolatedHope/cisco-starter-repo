dc27b4168987b8b356b4e675eaff0172bd07e2da
Kalidas <110555858+DesolatedHope@users.noreply.github.com>
2023-06-29 16:39:42 +0530

    Two components created

---
+++ sextant/src/Cards.js
@@ -0,0 +1,10 @@
+import './Cards.css';
+const Cards = () => {
+    return ( 
+        <div className = "Cards">
+            <h1 className = "Cards-header">Component</h1>
+        </div>
+     );
+}
+ 
+export default Cards;
